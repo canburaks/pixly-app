7f63a6234cfe6625e213e14df785578ff0f4b639
Can Burak S <canburak@msn.com>
2019-11-09 22:26:43 +0300

    ...

--- _FRONTEND/src/functions/third-party/facebook.js
+++ _FRONTEND/src/functions/third-party/facebook.js
@@ -117,7 +117,7 @@ function initFb(callback){
 		var script = document.createElement("script");
 		script.type = "text/javascript";
 		script.async = true;
-		script.src = "https://connect.facebook.net/en_US/sdk/debug.js";
+		script.src = "https://connect.facebook.net/en_US/sdk.js";
 		document.getElementsByTagName("head")[0].appendChild(script)
 	}
 }
