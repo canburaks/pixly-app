89dc13af6fff47eff45636b30d5dafe95ec96db2
Can Burak S <canburak@msn.com>
2019-12-06 14:20:36 +0300

    ...

--- _FRONTEND/webpack.config.js
+++ _FRONTEND/webpack.config.js
@@ -64,16 +64,16 @@ const productionSettings = {
 				// into invalid ecma 5 code. This is why the 'compress' and 'output'
 				// sections only apply transformations that are ecma 5 safe
 				// https://github.com/facebook/create-react-app/pull/4234
-				ecma: 5,
+				ecma: 8,
 			  },
 			  compress: {
 				ecma: 5,
-				warnings: true,
+				warnings: false,
 				// Disabled because of an issue with Uglify breaking seemingly valid code:
 				// https://github.com/facebook/create-react-app/issues/2376
 				// Pending further investigation:
 				// https://github.com/mishoo/UglifyJS2/issues/2011
-				comparisons: true,
+				comparisons: false,
 				// Disabled because of an issue with Terser breaking valid code:
 				// https://github.com/facebook/create-react-app/issues/5250
 				// Pending futher investigation:
