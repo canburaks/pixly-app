4d0727f2cc304775ee3beca825156504e681f037
Can Burak S <canburak@msn.com>
2020-03-03 17:06:05 +0300

    ...

--- persons/profile.py
+++ persons/profile.py
@@ -210,7 +210,7 @@ class Profile(SocialMedia, SEO):
             return Persona.objects.filter(user=self.user, id=self.user.id).defer("similars_dummy").first()
 
     def promote(self):
-        if self.points>=40 and scan_recommendations:
+        if self.points>=40:
             self.set_seo_description_keywords()
             MyQueue.put(self.sync_movie_archives)
             print("*")
